019ea2400fea4027e8349664ea340036aa948f7b
AL16F13 <julyev.2508.guerrero@gmail.com>
2015-12-05 13:46:51 -0600

    doce

--- js/acciones.js
+++ js/acciones.js
@@ -33,12 +33,12 @@ document.addEventListener("deviceready",function(){
 			data:"Marca="+$('txt_buscar').val(),
 			error: function(){alert("Error en la conexion");},
 			success: function(respuesta){var producto = JSON.parse(respuesta);
-			 $('#contenido').empty();
+			 $('#encontrar').empty();
 			for(var $x=0; $x<producto.perfumes.length; $x++)
 			{
-				$('#contenido').append('<div><div style=" display:inline-block"><div ><img src="http://192.168.1.185/perfumeria/recursos/imagenes/fotos/'+producto.perfumes[$x].IdPerfume+'"></div><div ><p>Clave: </p>'+ producto.perfumes[$x].IdPerfume +'</div></div><div style="display: inline-block"><div ><p>Nombre: </p>'+ producto.perfumes[$x].NombrePerfume +'</div><div ><p>Marca: </p>'+ producto.perfumes[$x].Marca +'</div><div ><p>Contenido: </p>'+ producto.perfumes[$x].Contenido +'</div><div ><p>Precio de Venta: $</p>'+ producto.perfumes[$x].PrecioVenta +'</div></div></div><br><hr>');
+				$('#encontrar').append('<div><div style=" display:inline-block"><div ><img src="http://192.168.1.185/perfumeria/recursos/imagenes/fotos/'+producto.perfumes[$x].IdPerfume+'"></div><div ><p>Clave: </p>'+ producto.perfumes[$x].IdPerfume +'</div></div><div style="display: inline-block"><div ><p>Nombre: </p>'+ producto.perfumes[$x].NombrePerfume +'</div><div ><p>Marca: </p>'+ producto.perfumes[$x].Marca +'</div><div ><p>Contenido: </p>'+ producto.perfumes[$x].Contenido +'</div><div ><p>Precio de Venta: $</p>'+ producto.perfumes[$x].PrecioVenta +'</div></div></div><br><hr>');
 			}
-			$(':mobile-pagecontainer').pagecontainer('change','#Enlistar',{
+			$(':mobile-pagecontainer').pagecontainer('change','#Buscar',{
 				transition: 'pop'});
 				
 			   }

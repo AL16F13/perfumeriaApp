40675694f58f6c037cbfa1372e2703f5f1b67404
AL16F13 <julyev.2508.guerrero@gmail.com>
2015-12-05 12:37:24 -0600

    cinco

--- js/acciones.js
+++ js/acciones.js
@@ -12,7 +12,7 @@ document.addEventListener("deviceready",function(){
 			 $('#contenido').empty();
 			for(var $x=0; $x<producto.perfumes.length; $x++)
 			{
-				$('#contenido').append('<div><div style=" display:inline-block"><div ><img src="perfumeria/recursos/imagenes/fotos/1.jpg"></div><div >'+ producto.perfumes[$x].IdPerfume +'</div></div><div style="display: inline-block"><div >'+ producto.perfumes[$x].NombrePerfume +'</div><div >'+ producto.perfumes[$x].Marca +'</div><div >'+ producto.perfumes[$x].Contenido +'</div><div >'+ producto.perfumes[$x].PrecioVenta +'</div></div></div>');
+				$('#contenido').append('<div><div style=" display:inline-block"><div ><img src="http://192.168.1.184/recursos/imagenes/fotos/'+producto.perfumes[$x].IdPerfume+'"></div><div >'+ producto.perfumes[$x].IdPerfume +'</div></div><div style="display: inline-block"><div >'+ producto.perfumes[$x].NombrePerfume +'</div><div >'+ producto.perfumes[$x].Marca +'</div><div >'+ producto.perfumes[$x].Contenido +'</div><div >'+ producto.perfumes[$x].PrecioVenta +'</div></div></div>');
 			}
 			$(':mobile-pagecontainer').pagecontainer('change','#Enlistar',{
 				transition: 'pop'});
